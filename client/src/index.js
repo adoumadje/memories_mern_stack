11059af79b733d3048c915f3b30e7f3c0583020c
adoumadje <adoumadjecaleb@gmail.com>
2022-07-11 03:27:03 +0300

    redux 53mn

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+
+import reducers from './reducers'
 
 import App from './App'
 
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 createRoot(document.getElementById('root')).render(<App />)
