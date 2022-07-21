2d3b92e89632399e0d3cee5b37231503536dbdd8
adoumadje <adoumadjecaleb@gmail.com>
2022-07-21 12:46:03 +0300

    2h54

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Avatar, Container, Grid, Paper, Typography } from '@mui/material'
+import LockOpenIcon from '@mui/icons-material/LockOpen'
+
+import { styles } from './styles'
 
 function Auth() {
-    const state = null
+  const state = null
+  const isSignup = false
+
+  function handleSubmit() {
+
+  }
 
   return (
-    <div>Auth</div>
+    <Container component='main' maxWidth='xs'>
+      <Paper sx={styles.paper} elevation={3}>
+        <Avatar sx={styles.avatar}>
+          <LockOpenIcon />
+        </Avatar>
+        <Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+        <form style={styles.form} onSubmit={handleSubmit}>
+          <Grid>
+
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
   )
 }
 
